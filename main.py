e77ed87e66db6cd321779abc5f563c6d7cb5814e
Ramu Gautam <gautamramu01@gmail.com>
2022-05-31 12:56:34 -0700

    test notebook added

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import math as math
 import os
 from PIL import Image
 from tifffile import imsave
-
+import scipy.io as scio
+import glob as glob
+from skimage.transform import resize
 
 if __name__ == '__main__':
     # takes the niftii, saves the tif's, saves the 3D images of each channel at all time points
@@ -71,24 +73,71 @@ if __name__ == '__main__':
 
             del sliceB, slice2, slice1, sliceA, originalImage,originalImageFloat32 #clear; free up some memory
 
-########################################################################################################################
+    ####################################################################################################################
 
-    fadf
+    colormap = scio.loadmat('C:/my3D_matlab/colormap.mat')
+    t1 = 1;
+    t2 = 41;
 
+    # size of image, size of cuboids
+    I3dw = [512, 280, 15]
+    I3d = [35, 35, I3dw(2)]
 
+    for time in range(t1-1,t2):
+        print(time)
+        tt = str(time)
+        addr = 'D:/NEW\Prediction_Result_Ajuba_09/Prediction_Dataset_Ajuba_sqh-cherry_jub-gfp 18A-09/FC-DenseNet/' + str(time) + '/'
+        addr2 = 'D:/NEW/Recombined_18A-09/'+ str(time) + '/'
 
+        if not os.path.isdir(addr2):
+            os.makedirs(addr2)
+        Files1 = glob.glob(addr + '*.nii')
+        print(Files1)
+        Fullsize = np.zeros(512, 280, 15)
+        Fullsize_regression = np.zeros(512, 280, 15)
+        Fullsize_input = np.zeros(512, 280, 15)
+        Weights = np.zeros(512, 280, 15, 64)
 
+        c_file = 0
 
+        for i1 in range(0,I3dw(0)-I3d(0),I3d(0)):
+            for i2 in range(0,I3dw(1)-I3d(1),I3d(1)):
+                V = nib.load(Files1(c_file))
+                V_arr = np.asarray(nib.load(Files1[c_file]).dataobj).astype(np.float32).squeeze()
+                V_arr = 1-V_arr
+                V2 = np.uint8(V_arr*255)
+                # v3 = imresize3(V2,I3d,'linear')
+                V3 = resize(V2,I3d,order=1)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# for i1=1:I3d(1): I3dw(1) - I3d(1) + 1
+# for i2=1:I3d(2): I3dw(2) - I3d(2) + 1
+# V = niftiread(strcat(addr, Files1(c_file).name));
+# V = 1 - V; % Because
+# the
+#
+#
+# class dictionary was defined that way (background =1 and foreground =0)
+#
+#
+# V2 = uint8(V * 255);
+# V3 = imresize3(V2, I3d, 'linear');
+#
+# a = i1;
+# b = i1 + I3d(1) - 1;
+# c = i2;
+# d = i2 + I3d(2) - 1;
+# Fullsize(a: b, c: d,:)=V3;
+#
+# V = niftiread(strcat(addr, Files1(c_file + 1).name));
+# for iy=1:64 % % all = 256
+# V2 = double(V(:,:,:, iy));
+# V3 = imresize3(V2, I3d, 'linear');
+# Weights(a: b, c: d,:, iy)=V2;
+# end
+#
+# V = niftiread(strcat(addr, Files1(c_file + 2).name));
+# V3 = imresize3(V, I3d, 'linear');
+# Fullsize_input(a: b, c: d,:)=V3;
+# c_file = c_file + 4;
+# end
+# end
