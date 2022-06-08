f89482341431f8a1eeebefccbe13875b429a7d22
Ramu Gautam <gautamramu01@gmail.com>
2022-06-07 18:36:18 -0700

    initialize steps

--- prepare.py
+++ prepare.py
@@ -1,4 +1,3 @@
-
 # import cv2
 import glob as glob
 import math as math
@@ -14,7 +13,8 @@ from PIL import Image
 from skimage.transform import resize
 from tifffile import imsave
 
-if __name__ == '__prepare__':
+
+def prepare():
     # takes the niftii, saves the tif's, saves the 3D images of each channel at all time points
     time = 1;
     z = 15;
@@ -29,8 +29,8 @@ if __name__ == '__prepare__':
     protein1name = 'Ecad';
     protein2name = 'Myosin';
 
-    if not os.path.isdir(originalImageAddress+"3DImage"):
-        os.makedirs(originalImageAddress+"3DImage")
+    if not os.path.isdir(originalImageAddress + "3DImage"):
+        os.makedirs(originalImageAddress + "3DImage")
 
     dirp1 = originalImageAddress + '3DImage/' + originalImageName + '/' + protein1name
     dirp2 = originalImageAddress + '3DImage/' + originalImageName + '/' + protein2name
@@ -42,10 +42,10 @@ if __name__ == '__prepare__':
 
     print(originalImageSize)
 
-    for i in range (0,originalImageSize[3]):
-        if(i<9):
+    for i in range(0, originalImageSize[3]):
+        if (i < 9):
             ttag = '000'
-        elif (i<99):
+        elif (i < 99):
             ttag = '00'
         else:
             ttag = '0'
@@ -56,10 +56,10 @@ if __name__ == '__prepare__':
         nib.save(slice1, dirp1 + '/threeD_img' + ttag + str(i + 1))
         nib.save(slice2, dirp2 + '/threeD_img' + ttag + str(i + 1))
 
-        for j in range (0,originalImageSize[2]):
-            if(j<9):
+        for j in range(0, originalImageSize[2]):
+            if (j < 9):
                 ztag = '000'
-            elif (j<99):
+            elif (j < 99):
                 ztag = '00'
             else:
                 ztag = '0'
@@ -67,13 +67,13 @@ if __name__ == '__prepare__':
             tifname1 = dirp1 + '/' + originalImageName + '_t' + ttag + str(i + 1) + '_z' + ztag + str(j + 1) + '.tif'
             tifname2 = dirp1 + '/' + originalImageName + '_t' + ttag + str(i + 1) + '_z' + ztag + str(j + 1) + '.tif'
 
-            Xxxx = Image.fromarray(originalImageFloat32[:,:,j,i,0],mode='F')
+            Xxxx = Image.fromarray(originalImageFloat32[:, :, j, i, 0], mode='F')
             sliceA = Image.fromarray(np.asarray(Xxxx).squeeze())
             sliceA.save(tifname1)
-            Xxxx = Image.fromarray(originalImageFloat32[:,:,j,i,0],mode='F')
+            Xxxx = Image.fromarray(originalImageFloat32[:, :, j, i, 0], mode='F')
             sliceB = Image.fromarray(np.asarray(Xxxx).squeeze())
             sliceB.save(tifname2)
 
-    del sliceB, slice2, slice1, sliceA, originalImage,originalImageFloat32 #clear; free up some memory
+    del sliceB, slice2, slice1, sliceA, originalImage, originalImageFloat32  # clear; free up some memory
 
     ####################################################################################################################
